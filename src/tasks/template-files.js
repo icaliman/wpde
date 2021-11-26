02129ee9e1cde95121a6886b7e74f1fde3b8b80d
Ion <icaliman92@gmail.com>
2021-11-26 11:52:09 +0200

    fix async content replace

--- src/tasks/template-files.js
+++ src/tasks/template-files.js
@@ -23,6 +23,29 @@ async function replaceAsync(str, regex, asyncFn) {
     return str.replace(regex, () => data.shift());
 }
 
+async function replacePattern(cont, pattern) {
+    if (pattern.match) {
+        let matchEscaped = pattern.match.replace(
+            /([.*+?^${}()|[\]/\\])/g,
+            "\\$1"
+        );
+
+        // Match custom patterns
+        if (matchEscaped.endsWith("\\*")) {
+            matchEscaped = matchEscaped.replace("\\*", "[.\\w\\d_-]+");
+        }
+
+        const matchRegExp = new RegExp(`@@${matchEscaped}`, "g");
+
+        if ("function" === typeof pattern.replacement) {
+            cont = await replaceAsync(cont, matchRegExp, pattern.replacement);
+        } else {
+            cont = cont.replace(matchRegExp, pattern.replacement || "");
+        }
+    }
+    return cont;
+}
+
 /**
  * Replace patterns.
  *
@@ -32,35 +55,20 @@ async function replaceAsync(str, regex, asyncFn) {
  * @return {*} Content with replaced patterns
  */
 function replacePatterns(cont, patterns, cb) {
-    let promises = patterns.map(async (pattern) => {
-        if (pattern.match) {
-            let matchEscaped = pattern.match.replace(
-                /([.*+?^${}()|[\]/\\])/g,
-                "\\$1"
-            );
+    let index = 0;
 
-            // Match custom patterns
-            if (matchEscaped.endsWith("\\*")) {
-                matchEscaped = matchEscaped.replace("\\*", "[.\\w\\d_-]+");
-            }
-
-            const matchRegExp = new RegExp(`@@${matchEscaped}`, "g");
-
-            if ("function" === typeof pattern.replacement) {
-                cont = await replaceAsync(
-                    cont,
-                    matchRegExp,
-                    pattern.replacement
-                );
-            } else {
-                cont = cont.replace(matchRegExp, pattern.replacement || "");
-            }
+    function _replace_next() {
+        if (index >= patterns.length) {
+            return cb(null, cont);
         }
-    });
 
-    Promise.all(promises).then(() => {
-        cb(null, cont);
-    });
+        replacePattern(cont, patterns[index]).then((newCont) => {
+            cont = newCont;
+            index++;
+            _replace_next();
+        });
+    }
+    _replace_next();
 }
 
 module.exports = {
